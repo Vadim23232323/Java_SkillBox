12373417e70fde95acace5526609e5fd93a4cdef
Vadim <Vadim_milo.net@mail.ru>
2023-01-22 13:27:46 +0300

    Обьекты и классы

---
+++ Lessons_3/NumbersTypesValues/src/Numbers.java
@@ -0,0 +1,16 @@
+public class Numbers {
+    public static void main(String[] args) {
+
+        Double maxD = Double.MAX_VALUE;
+
+        Double minD = Double.MIN_VALUE;
+
+        Float maxF = Float.MAX_VALUE;
+
+        Float minF = Float.MIN_VALUE;
+
+        Integer maxI = Integer.MAX_VALUE;
+
+        Integer minI = Integer.MIN_VALUE;
+    }
+}
