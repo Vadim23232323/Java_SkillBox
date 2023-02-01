5f5420fac1e48c6bd04f989b289a9e1a2203f1c7
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2023-02-01 16:38:15 +0300

    Массивы и коллекции

--- Lessons_4/Array/src/Array_list.java
+++ Lessons_4/Array/src/Array_list.java
@@ -1,76 +1,130 @@
-import javax.xml.crypto.dsig.spec.XPathFilterParameterSpec;
 import java.util.Scanner;
+import java.util.ArrayList;
 
+// Задание 5.4 Создает список дел, который управляется командами в консоли. Команды: LIST, ADD, EDIT, DELETE.
 public class Array_list {
-    String text = "Каждый охотник желает знать, где сидит фазан.";
 
-    //Задание 5.1 Вывод массива в обратной последовательности
-    public void getArrayViceVersa() {
-        String[] word = text.split(",?\\.?\\s+?");
-        for (int i = word.length - 1; i >= 0; i--) {
-            System.out.println("Слово № " + i + ": " + word[i]);
+    // Задает зеленый цвет текста
+    public static final String ANSI_GREEN = "\u001B[32m";
+    // Задает стандартный цвет текста
+    public static final String ANSI_RESET = "\u001B[0m";
+    ArrayList<String> todoList = new ArrayList<>();
+    Scanner scanIndex = new Scanner(System.in);
+    Scanner scanElement = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+    String todoElement;
+    int todoIndex = 0, command = 0;
+
+    //Меню
+    public void getMenu() {
+        System.out.println("Меню команд: " + ANSI_GREEN + "\" \n\t 1. LIST " + ANSI_RESET + "- Выводит список дел " +
+                ANSI_GREEN + "\" \n\t 2. ADD " + ANSI_RESET + "- Добавляет запись в список дел " +
+                ANSI_GREEN + "\" \n\t 3. EDIT " + ANSI_RESET + "- Изменяет запись в списке дел " +
+                ANSI_GREEN + "\" \n\t 4. DELETE " + ANSI_RESET + "- Удаляет запись списке " +
+                ANSI_GREEN + "\" \n\t 5. DELETE-ALL " + ANSI_RESET + "- Удаляет все записи в списке дел" +
+                ANSI_GREEN + "\" \n\t 6. EXIT " + ANSI_RESET + "- Выход из приложения.");
+        setCommand();
+    }
+
+    //Ввод с консоли номера команды
+    public void setCommand() {
+
+        command = 0;
+
+        System.out.println("Введите № команды: ");
+
+        command = scanner.nextInt();
+
+        switch (command) {
+            case 1:
+                printArrayList();
+                break;
+            case 2:
+                setAddArrayList();
+                getMenu();
+            case 3:
+                setEditArrayList();
+                getMenu();
+            case 4:
+                setDeleteArrayList();
+                getMenu();
+            case 5:
+                setDeleteAllArrayList();
+                getMenu();
+            case 6:
+                System.out.println("-------------------------------------------------------------------");
+                System.out.println("Приложение завершило работу.");
+                System.out.println("-------------------------------------------------------------------");
+                break;
+            default:
+                if (command < 0 || command > 6) {
+                    System.out.println("Введена неверная команда! Введите команду повторно.");
+                }
+                getMenu();
         }
     }
 
-    //Задание 5.2 Создает массив типа float с температурами 30 пациентов (от 32 до 40 градусов).
-    public void getPatientsTemperature() {
-        float[] patientsTemperature = new float[30];
-        int countHealthy = 0;
-        float averageTemperature = (float) 0.0;
-        float maxTemperature = (float) 0.0;
-        float minTemperature = (float) 0.0;
-
-        for (int i = 0; i < patientsTemperature.length; i++) {
-            patientsTemperature[i] = (float) (Math.round(((Math.random() * 8) + 32) * 10.0) / 10.0);
-            minTemperature = patientsTemperature[0];
-            System.out.println("Температура пациента " + i + ": " + patientsTemperature[i]);
-
-            if (patientsTemperature[i] >= 36.2 & patientsTemperature[i] <= 36.9) {
-                countHealthy++;
-            }
-            if (patientsTemperature[i] > maxTemperature) {
-                maxTemperature = patientsTemperature[i];
-            }
-
-            if (patientsTemperature[i] < minTemperature) {
-                minTemperature = patientsTemperature[i];
-            }
-            averageTemperature = (averageTemperature + patientsTemperature[i]);
-        }
-        System.out.println("Количество здоровых пациентов больницы: " + countHealthy);
-        System.out.println("Средняя температура пациентов по больнице: " + averageTemperature / patientsTemperature.length);
-        System.out.println("Максимальная температура пациента по больнице: " + maxTemperature);
-        System.out.println("Минимальная температура пациента по больнице: " + minTemperature);
-
+    // Добавление записи в ArrayList из консоли
+    public ArrayList<String> setAddArrayList() {
+        System.out.println("Введите № записи");
+        todoIndex = scanIndex.nextInt();
+        System.out.println("Введите название записи: ");
+        todoElement = scanElement.nextLine();
+        todoList.add(todoIndex, todoElement);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Запись " + ANSI_GREEN + "№ " + todoIndex + " : " + todoElement + ANSI_RESET + " добавлена в список ");
+        System.out.println("-------------------------------------------------------------------");
+        command = 0;
+        return todoList;
     }
 
-    //Задание 5.3 Создает с помощью циклов двумерный массив строк. При его распечатке в консоли выводиться крестик из X.
-    public void getX() {
-        String[] []  x = new String[7] [7];
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x.length; j++) {
-                if (i == j || x.length - i - 1 == j)
-                    x [i] [j] = "X";
-                else
-                    x [i] [j] = " ";
-            }
-            x [i] [i] = "X";
-        }
+    // Изменение записи в ArrayList из консоли
+    public ArrayList<String> setEditArrayList() {
 
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x.length; j++) {
-                System.out.print(x[i][j] + " " );
-            }
-            System.out.println();
-        }
-
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x.length; j++) {
-                System.out.print("Номер элемента массива: " + i + " " + j + " " );
-            }
-            System.out.println();
-        }
-
-
+        System.out.println("Введите № записи которую хотите заменить:");
+        todoIndex = scanIndex.nextInt();
+        System.out.println("Введите новое название записи: ");
+        todoElement = scanElement.nextLine();
+        todoList.set(todoIndex, todoElement);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Запись " + ANSI_GREEN + todoElement + ANSI_RESET + " заменена в списке ");
+        System.out.println("-------------------------------------------------------------------");
+        getMenu();
+        return todoList;
     }
+
+    // Удаление записи из ArrayList
+    public ArrayList<String> setDeleteArrayList() {
+        System.out.println("Введите № записи которую хотите удалить:");
+        todoIndex = scanIndex.nextInt();
+        todoList.remove(todoIndex);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Запись " + ANSI_GREEN + todoElement + ANSI_RESET + " удалена из списка ");
+        System.out.println("-------------------------------------------------------------------");
+        return todoList;
+    }
+
+    // Удаление всех записей из ArrayList
+    public ArrayList<String> setDeleteAllArrayList() {
+        todoList.removeAll(todoList);
+        System.out.println("Список удален ");
+        return todoList;
+    }
+
+    // Вывод в консоль списка дел
+    public void printArrayList() {
+
+        System.out.println(ANSI_GREEN + " Список дел: " + ANSI_RESET);
+        System.out.println("-------------------------------------------------------------------");
+        if (todoList.size() == 0) {
+            System.out.println("СПИСОК ПУСТ!!! Добавьте запись в список.");
+        } else {
+            for (int i = 0; i < todoList.size(); i++) {
+                System.out.println("Запись №" + i + ": " + todoList.get(i));
+            }
+        }
+        System.out.println("-------------------------------------------------------------------");
+        getMenu();
+    }
+
 }
--- Lessons_4/Array/src/Loader.java
+++ Lessons_4/Array/src/Loader.java
@@ -1,17 +1,21 @@
 public class Loader {
     public static void main(String[] args) {
 
-        //Задание 5.1 Вывод массива в обратной последовательности
+        // Задание 5.1 Вывод массива в обратной последовательности
 //        Array_list words = new Array_list();
 //        words.getArrayViceVersa();
 
-        //Задание 5.2 Создает массив типа float с температурами 30 пациентов (от 32 до 40 градусов).
+        // Задание 5.2 Создает массив типа float с температурами 30 пациентов (от 32 до 40 градусов).
 //        Array_list pacient = new Array_list();
 //        pacient.getPatientsTemperature();
 
-        //Задание 5.3 Создает с помощью циклов двумерный массив строк. При его распечатке в консоли выводиться крестик из X.
-        Array_list xxx = new Array_list();
-        xxx.getX();
+        // Задание 5.3 Создает с помощью циклов двумерный массив строк. При его распечатке в консоли выводиться крестик из X.
+//        Array xxx = new Array();
+//        xxx.getX();
+
+        // Задание 5.4 Создает список дел, который управляется командами в консоли. Команды: LIST, ADD, EDIT, DELETE.
+        Array_list array_list = new Array_list();
+        array_list.getMenu();
 
     }
 }
