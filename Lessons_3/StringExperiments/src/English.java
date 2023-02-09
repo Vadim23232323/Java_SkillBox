59a3680c9e0fc1426a4cfdcdb5d6ee2212ef8ec5
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2023-02-09 18:29:59 +0300

    Объекты и классы

--- Lessons_3/StringExperiments/src/English.java
+++ Lessons_3/StringExperiments/src/English.java
@@ -5,7 +5,7 @@ public class English {
             "\n" +
             "Our neighborhood is safe, but Minnie is still quite cautious. She does not let strangers pet her, and she always rushes back home if she notices a dog. My father made some kind of ladder for Minnie, so she can easily go out and come back. Minnie is a smart cat. I have a feeling she understands everything I say to her. She is pretty active and loves playing. Her favorite game is to wake us up at 4 a.m. by running and jumping around the apartment. Minnie is a picky eater. I know what kind of food she prefers, and she knows that I know. If for whatever reason I gave her, for example tuna (she does not like tuna), she would ignore it and demand her favorite treat.\n" +
             "\n" +
-            "As most cats do, Minnie sleeps a lot during the day. We got her a cat house for her Birthday, but she prefers to sleep on my keyboard, on my father’s desk when he is working, on my mom’s lap when she is watching TV. Although, I saw her a few times sleeping in her house.\n" +
+            "As most cats do, Minnie sleeps a lot during the day. We got her a cat house for her Birthday, but she prefers to sleep on my keyboard, on my father’s desk when he is working, on my mom’s lap when she is watching TV. Although, I saw her a few time sleeping in her house.\n" +
             "\n" +
             "Minnie is a beautiful cat, but I love her not for her looks. She is my good friend, and I consider her a family member. Some people say that dogs make good friends but cats do not. I would not agree with that. Neither would my cat.";
 
