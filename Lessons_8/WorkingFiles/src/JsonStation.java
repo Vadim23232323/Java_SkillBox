6fe9b6d55f3ed31ed693ba53302acd78f3e96325
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2023-03-15 17:24:55 +0300

    09.Работа с файлами и сетью

---
+++ Lessons_8/WorkingFiles/src/JsonStation.java
@@ -0,0 +1,31 @@
+public class JsonStation
+{
+    private String nameStation;
+    private String numberStation;
+
+    public JsonStation(String numberStation,String nameStation) {
+        this.numberStation = numberStation;
+        this.nameStation = nameStation;
+    }
+
+    public String getNameStation() {
+        return nameStation;
+    }
+
+    public void setNameStation(String nameStation) {
+        this.nameStation = nameStation;
+    }
+
+    public String getNumberStation() {
+        return numberStation;
+    }
+
+    public void setNumberStation(String numberStation) {
+        this.numberStation = numberStation;
+    }
+
+
+
+
+
+}
--- Lessons_8/WorkingFiles/src/WorkFileJson.java
+++ Lessons_8/WorkingFiles/src/WorkFileJson.java
@@ -6,71 +6,85 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WorkFileJson {
+    private ArrayList<JsonStation> stations = new ArrayList<>();
 
-
-
-
-
-//    // Парсим HTML файл с помощью библиотеки Jsoup и делаем выборку в файле
-//    public void parseHtmlFile () {
-//        Document doc = Jsoup.parse(loadHtmlFile());
-//        Elements elements =  doc.select("li.start-screen-directions__item");
-//        System.out.println(Menu.ANSI_GREEN + "--------------------------------------------------------" + Menu.ANSI_RESET);
-////        elements.forEach(System.out::println);
-//        elements.forEach(element -> {
-//            System.out.println(element.text());
-//        });
-//        System.out.println(Menu.ANSI_GREEN + "--------------------------------------------------------" + Menu.ANSI_RESET);
-//    }
-
-//    // Парсим HTML код страницы с помощью библиотеки Jsoup и делаем выборку в файле
-//    public void parseHtmlSite () throws IOException {
-//        Document doc = Jsoup.connect("https://www.moscowmap.ru/metro.html#lines")
-//                .maxBodySize(0)
-//                .userAgent("Chrome/110.0.5481.180")
-//                .referrer("http://www.google.com")
-//                .get();
-//        Elements elements =  doc.select("div#metrodata ");
-//
-//        for (Element element: elements) {
-//            try {
-//                    System.out.println(element.text());
-//
-//            } catch (Exception ex) {
-//                System.out.println("Произошла ошибка при чтении URL, картинка не будет загружена!");
-//            }
-//
-//        }
-//    }
+    public void getPrintStation () {
+        for (JsonStation station: stations) {
+            System.out.println("Номер: " + station.getNumberStation());
+        }
+    }
 
 
     // Парсим HTML код страницы с помощью библиотеки Jsoup и делаем выборку в файле
-    public void parseHtmlSite () throws IOException {
-        Document doc = Jsoup.connect("https://www.moscowmap.ru/metro.html#lines")
+    public ArrayList<JsonStation> parseHtmlSite () throws IOException {
+        Document doc = Jsoup.connect("https://skillbox-java.github.io/")
+                .maxBodySize(0)
                 .userAgent("Chrome/110.0.5481.180")
                 .referrer("http://www.google.com")
                 .get();
 
-        Elements elements =  doc.select("span.name");
+        Elements elements =  doc.select("span.num, span.name");
 
         for (Element element: elements) {
-            Element link = element;
-            String absSrc = link.text();
-
+            String nameStation = element.select("span.name").text();
+            String numStation = element.select("span.num").text();
+            // stations.add(new JsonStation(numStation,nameStation));
             try {
-                if (absSrc.length() > 0) {
-                    System.out.println("-------------------------");
-                    System.out.println(absSrc);
-                }
+           // System.out.println(numStation + " " + nameStation);
+                System.out.println(element.text());
+
             } catch (Exception ex) {
                 System.out.println("Произошла ошибка при чтении URL, картинка не будет загружена!");
             }
 
         }
+        return stations;
     }
+
+
+
+
+    // Загружаем CSV выписку
+//    public  ArrayList<BankStatement> loadCsvFile()
+//    {
+//        try
+//        {
+//            List<String> lines = Files.readAllLines(Paths.get(pathCsvFile));
+//            for(String line : lines)
+//            {
+//                String[] fragments = line.split(",");
+//                if(fragments.length != 8) {
+//                    System.out.println("Не удается прочитать строку выписки: " + line);
+//                    continue;
+//                }
+//                statements.add(new BankStatement(
+//                        fragments[0],
+//                        fragments[1],
+//                        fragments[2],
+//                        (new SimpleDateFormat(dateFormat)).parse(fragments[3]),
+//                        fragments[4],
+//                        fragments[5],
+//                        Integer.parseInt(fragments[6]),
+//                        Integer.parseInt(fragments[7])
+//                ));
+//
+//
+//            }
+//
+//            System.out.println(Menu.ANSI_GREEN + "--------------------------------------------------------" + Menu.ANSI_RESET);
+//            System.out.println("Выписка " + pathCsvFile + " загружена!!!");
+//            System.out.println(Menu.ANSI_GREEN + "--------------------------------------------------------" + Menu.ANSI_RESET);
+//        }
+//        catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//        return statements;
+//    }
 
 
 }
